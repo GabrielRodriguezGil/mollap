3ddb4b9e904a4c5f2027224d5d30aec77f16613a
goskus23 <141594539+goskus23@users.noreply.github.com>
2026-03-01 18:41:41 +0100

    feat(tareas): :sparkles: Añadir la clase tareas

---
+++ src/administrador/tareas.py
@@ -0,0 +1,27 @@
+class tareas():
+    
+    def __init__(self):
+        self.tareas = []
+        self.target = None
+    
+    def get_tareas(self):
+        return self.tareas
+    
+    def get_target(self):
+        return self.target
+    
+    def set_target(self, vehicle):
+        self.target = vehicle
+
+    def add_tarea(self,tarea):
+        self.get_tareas.append(tarea)
+
+    def ejecución(self,id):
+        for tarea in self.get_tareas():
+            tarea.ejecucion()
+
+        if self.target is not None:
+            self.target.ejecucion()
+        else:
+            pass
+        
