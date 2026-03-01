d1bb7354da539223ac465965b22489e1c7746de9
goskus23 <141594539+goskus23@users.noreply.github.com>
2026-03-01 18:53:30 +0100

    feat(programador_tareas): :sparkles: Añadir la clase programador de tareas.

---
+++ src/administrador/programador_tareas.py
@@ -0,0 +1,20 @@
+
+from src.administrador.tareas import Tareas
+
+class Progrmador_tareas():
+
+    def __init__(self):
+        self.tareas = []
+
+    def programar_tarea(self, target):
+        tareas = Tareas()
+        tareas.set_target(target)
+
+    def get_tareas(self):
+        return self.tareas
+    
+    def set_tarea(self, tarea):
+        self.get_tareas.add_tarea(tarea)
+    
+    def ejecutar_tarea(self, id):
+        self.tareas.ejecucion(id)
